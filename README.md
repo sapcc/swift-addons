07cb3adeea737a4e22879960a802a8e5e7d901a9
Stefan Majewsky <stefan.majewsky@sap.com>
2017-02-27 13:37:22 +0100

    add implementation for sysmeta-domain-override middleware

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Then follow the instructions for the specific middlewares as shown below.
 
 ### sysmeta-domain-override
 
-Our [Swift] is configured for manual account creation such that all accounts are
-created by an automated process (and assigned reasonable quotas in the same step). Because the reseller user who
-performs the account creation resides in a different domain than customer projects, accounts created by it will not have
-the correct `X-Account-Sysmeta-Project-Domain-Id` attribute (which we need for our internal bookkeeping).
+Our Swift is configured for manual account creation such that all accounts are created by an automated process (and
+assigned reasonable quotas in the same step). Because the reseller user who performs the account creation resides in a
+different domain than customer projects, accounts created by it will not have the correct
+`X-Account-Sysmeta-Project-Domain-Id` attribute (which we need for our internal bookkeeping).
 
 This middleware solves this problem by allowing reseller users to specify the domain ID for the project in question
 explicitly with the `X-Account-Project-Domain-Id-Override` header.
--- sapcc_swift_addons/sysmeta_domain_override.py
+++ sapcc_swift_addons/sysmeta_domain_override.py
@@ -1,3 +1,23 @@
+# Copyright 2017 SAP SE
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from swift.common.request_helpers import get_sys_meta_prefix
+from swift.common.swob import Request
+
+SYSMETA_HEADER  = get_sys_meta_prefix('account') + 'project-domain-id'
+OVERRIDE_HEADER = 'x-account-project-domain-id-override'
+
 class DomainOverrideMiddleware(object):
     def __init__(self, app):
         self.app = app
@@ -10,4 +30,11 @@ class DomainOverrideMiddleware(object):
         return _factory
 
     def __call__(self, environ, start_response):
+        # only reseller may invoke the override
+        if environ.get('reseller_request', False):
+            req = Request(env)
+            if OVERRIDE_HEADER in req.headers:
+                req.headers[SYSMETA_HEADER] = req.headers[OVERRIDE_HEADER]
+
+        # we now continue with our regularly scheduled programming
         return self.app(environ, start_response)
